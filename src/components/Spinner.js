cd3c2f505f4b53f7d4519951cdab39918fb8a654
varid6 <varid68@gmail.com>
2020-03-04 14:33:48 +0700

    add proptypes spinner

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,31 +2,28 @@ import React from 'react'
 import { ActivityIndicator } from 'react-native'
 import { YELLOW } from '../constants/Colors'
 import { ITEMS_CENTER } from '../constants/Styles'
+import PropTypes from 'prop-types'
 
-/**
- * Animated spinner to indicate loading
- * @param {object=} style - Custom styles for the spinner
- * @param {boolean} [noDefaultStyle=false] - Bypasses the default styles
- */
-export default class Spinner extends React.Component {
-  setStyle() {
-    if (!this.props.noDefaultStyle) {
-      return {
-        ...ITEMS_CENTER,
-        height: 60
-      }
-    }
-    return null
-  }
-
-  render() {
-    return (
-      <ActivityIndicator
-        animating={true}
-        style={[this.setStyle(), this.props.style]}
-        size={this.props.size ? this.props.size : 'large'}
-        color={this.props.color ? this.props.color : YELLOW}
-      />
-    )
-  }
+const propsTypes = {
+  size: PropTypes.string,
+  color: PropTypes.string
 }
+
+const propsDefault = {
+  size: 'large',
+  color: YELLOW
+}
+
+const Spinner = ({ size, color }) => (
+  <ActivityIndicator
+    animating={true}
+    style={{ ...ITEMS_CENTER, height: 50 }}
+    size={size}
+    color={color} />
+)
+
+Spinner.propTypes = propsTypes
+
+Spinner.defaultProps = propsDefault
+
+export default Spinner
